2b55178cefd6653183fe90196ea450ffc019c717
Ness <bur>
2024-09-05 19:01:01 +0300

    Добавьте файлы проекта.

---
+++ Lesson3_4/Lesson3_4.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+using namespace std;
+#include <string>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int exp;
+    int level = 1;
+    string prikol;
+    cout << "Введите число очков опыта: ";
+    cin >> exp;
+    cout << "-----Считаем-----\n";
+    if (exp < 1000) {
+        cout << "Ваш уровень: " << level;
+    }
+    else if (exp >= 1000 && exp < 2500) {
+        cout << "Ваш уровень: " << level++;
+    }
+    else if (exp >= 2500 && exp < 5000) {
+        cout << "Ваш уровень: ";
+        level = 3;
+        cout << level;
+    }
+    else  {
+        cout << "Уровень: Бог";
+    }
+    
+
+
+}
+
